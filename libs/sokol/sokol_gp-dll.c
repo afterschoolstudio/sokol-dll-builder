8235bfa3b34688e433ba03aed462b2430527f1ff
Kyle Kukshtel <kyle.kukshtel@gmail.com>
2022-08-06 18:22:34 -0400

    working output of sokol_gp.dll and sokol.dll

--- libs/sokol/sokol_gp-dll.c
+++ libs/sokol/sokol_gp-dll.c
@@ -4,6 +4,9 @@
 #if defined(_WIN32)
 #define SOKOL_LOG(s) OutputDebugStringA(s)
 #endif
+
+
+
 /* sokol 3D-API defines are provided by build options */
 #include "sokol_app.h"
 #include "sokol_args.h"
@@ -12,3 +15,4 @@
 #include "sokol_gfx.h"
 #include "sokol_glue.h"
 #include "sokol_time.h"
+#include "sokol_gp.h"
